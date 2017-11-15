e8896cfb2fe8c58e631f83c4def683c580658cb7
krzysiek nowakowski <n.krzysiek@gmail.com>
2017-11-15 19:33:04 +0100

    ver .1

--- src/main/scala/com/krisdrum/SimpleSparkApp2.scala
+++ src/main/scala/com/krisdrum/SimpleSparkApp2.scala
@@ -1,24 +1,25 @@
 package com.krisdrum
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SimpleSparkApp2 extends App {
-    val inputFile = """C:\Users\nkrzy\Scala\Spark\LearningSparkBigDataAnalysis\src\main\resources\vulgata.txt"""
-    val outputFile = """C:\Users\nkrzy\Scala\Spark\LearningSparkBigDataAnalysis\src\main\resources\vulgataOut.txt"""
 
-    val conf = new SparkConf().setMaster("local").setAppName("SimpleSparkApp1")
-    val sc = new SparkContext(conf)
+        val inputFile = args(0)
+        val outputFolder = args(1)
 
-    def wordSplit(string: String) = string.split("""\W+""")
-    val input = sc.textFile(inputFile)
+        val conf = new SparkConf().setMaster("local").setAppName("SimpleSparkApp1")
+        val sc = new SparkContext(conf)
+        def wordSplit(string: String) = string.split("""\W+""")
 
-    val wordCount = input
-      .flatMap(wordSplit)
-      .map(word => word -> 1)
-      .reduceByKey(_+_)
+        val input = sc.textFile(inputFile)
 
-    println(wordCount)
+        val wordCount: RDD[(String, Int)] = input
+          .flatMap(wordSplit)
+          .map(word => word -> 1)
+          .reduceByKey(_ + _)
 
-    wordCount.saveAsTextFile(outputFile)
+        wordCount foreach println
+        wordCount.saveAsTextFile(outputFolder)
 }
 
 
