111bce2ea22760d729aee18c736106c97a6cc0d3
Krzysztof Nowakowski <Krzysztof.Nowakowski@swisscom.com>
2019-04-03 21:47:53 +0200

    word count example

---
+++ src/main/scala/com/krisdrum/wordCount/WordCount.scala
@@ -0,0 +1,20 @@
+package com.krisdrum.wordCount
+
+import com.krisdrum.standaloneApplication.StandaloneHelloWorld.getClass
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount extends App{
+  val conf = new SparkConf().setMaster("local").setAppName(getClass.getSimpleName)
+  val sc = new SparkContext(conf)
+  val lines = sc.textFile("src/main/resources/vulgata.txt")
+
+  val result = lines
+    .flatMap(_.split(" "))
+    .map(w => w -> 1)
+    .reduceByKey{case (x,y) => x + y}
+    .sortBy(_._2, false)
+    .take(10)
+    .toSeq
+
+  println(result)
+}
