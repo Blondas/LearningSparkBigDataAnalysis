940069f94aa4a3634b3135da092540c962159053
Krzysztof Nowakowski <Krzysztof.Nowakowski@swisscom.com>
2019-04-04 23:25:19 +0200

    aggregate

---
+++ src/main/scala/com/krisdrum/commonTransformationsAndActions/Aggregate.scala
@@ -0,0 +1,21 @@
+package com.krisdrum.commonTransformationsAndActions
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Aggregate extends App {
+  val conf = new SparkConf().setMaster("local").setAppName(getClass.getSimpleName)
+  val sc = new SparkContext(conf)
+
+  val input = sc.parallelize(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+  val zero: (Int, Int) = 0 -> 0
+
+  def combine(acc: (Int, Int), value: Int) = (acc._1 + value, acc._2 + 1)
+
+  def reduce(acc1: (Int, Int), acc2: (Int, Int)) = (acc1._1 + acc2._1, acc1._2 + acc2._2)
+
+  val result: (Int, Int) = input.aggregate(zero)(combine, reduce)
+
+  println(result._1 / result._2.toDouble)
+}
--- src/main/scala/com/krisdrum/commonTransformationsAndActions/MapFunction.scala
+++ src/main/scala/com/krisdrum/commonTransformationsAndActions/MapFunction.scala
@@ -1,5 +1,6 @@
 package com.krisdrum.commonTransformationsAndActions
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MapFunction extends App {
@@ -7,7 +8,7 @@ object MapFunction extends App {
   val sc = new SparkContext(conf)
 
   val input = sc.parallelize(Seq("hello", "world", "hi"))
-  val result = input flatMap (_ split " ")
+  val result: RDD[String] = input flatMap (_ split " ")
 
   println (result.collect.toSeq)
 }
